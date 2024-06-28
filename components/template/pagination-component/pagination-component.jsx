19946fcb862ac6ae3f156909da1ce917836ba22a
alivolley <alicryptovolley@gmail.com>
2024-06-28 20:22:37 +0330

    transition added to pagination

--- components/template/pagination-component/pagination-component.jsx
+++ components/template/pagination-component/pagination-component.jsx
@@ -1,28 +1,39 @@
 'use client';
 
 import { usePathname, useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
 // MUI
 import { Pagination } from '@mui/material';
 
+// Components
+import BackdropLoading from '../backdrop-loading/backdrop-loading';
+
 function PaginationComponent({ searchParams, totalPage }) {
    const { push } = useRouter();
    const pathName = usePathname();
+   const [isPending, startTransition] = useTransition();
 
    const changePageHandler = (e, newValue) => {
-      const newSearchParams = { ...searchParams, page: newValue };
-      const params = new URLSearchParams(newSearchParams).toString();
-      push(`${pathName}?${params}`);
+      startTransition(() => {
+         const newSearchParams = { ...searchParams, page: newValue };
+         const params = new URLSearchParams(newSearchParams).toString();
+         push(`${pathName}?${params}`);
+      });
    };
 
    return (
-      <Pagination
-         variant="outlined"
-         shape="rounded"
-         count={totalPage}
-         onChange={changePageHandler}
-         page={Number(searchParams?.page) || 1}
-      />
+      <>
+         <Pagination
+            variant="outlined"
+            shape="rounded"
+            count={totalPage}
+            onChange={changePageHandler}
+            page={Number(searchParams?.page) || 1}
+         />
+
+         <BackdropLoading open={isPending} />
+      </>
    );
 }
 
